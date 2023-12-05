3f475759f653d9bff9d0aa7180b9d9f1a9a774f5
Yashin <yashinnaran@gmail.com>
2023-12-05 15:09:56 +0200

    Add: Created backdrop component , falls in behind accordion.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Main from "./components/Main";
 import FaqAccordion from "./components/faq accordion/FaqAccordion";
+import Backdrop from "./components/backdrop/Backdrop";
 
 function App({}) {
   return (
     <Main>
+      <Backdrop />
       <FaqAccordion />
     </Main>
   );
